86ae20dbcf7516a3311514877ae8120c9e011856
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-02-10 14:28:29 +0000

    First working version
    Fix attribute names/template variable names across the board.
    Fix the test cookbook.

--- providers/ports.rb
+++ providers/ports.rb
@@ -5,13 +5,17 @@ end
 use_inline_resources
 
 action :create do
+  directory '/etc/cumulus' do
+    action :create
+  end
+
   template '/etc/cumulus/ports.conf' do
-    cookbook 'cumulus-cl-ports-chef'
+    cookbook 'cumulus'
     source 'ports.erb'
     variables({
       :speed_10g => Cumulus::Utils.expand_port_list(new_resource.speed_10g),
       :speed_40g => Cumulus::Utils.expand_port_list(new_resource.speed_40g),
-      :speed_40_div_4g => Cumulus::Utils.expand_port_list(new_resource.speed_40_div_4g),
+      :speed_40g_div_4 => Cumulus::Utils.expand_port_list(new_resource.speed_40g_div_4),
       :speed_4_by_10g => Cumulus::Utils.expand_port_list(new_resource.speed_4_by_10g)
     })
     owner 'root'
--- resources/ports.rb
+++ resources/ports.rb
@@ -4,5 +4,5 @@ default_action :create
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :speed_10g, :kind_of => Array, :default => []
 attribute :speed_40g, :kind_of => Array, :default => []
-attribute :speed_40_div_4g, :kind_of => Array, :default => []
+attribute :speed_40g_div_4, :kind_of => Array, :default => []
 attribute :speed_4_by_10g, :kind_of => Array, :default => []
