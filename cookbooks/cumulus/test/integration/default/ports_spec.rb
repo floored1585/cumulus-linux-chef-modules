0e54c9294642bc6dd54fc2fb9987c1810f3077db
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-02-10 16:11:29 +0000

    Add Serverspec tests
    Add Serverspec to the Gemfile
    Add some simple Serverspec tests

---
+++ cookbooks/cumulus/test/integration/default/ports_spec.rb
@@ -0,0 +1,24 @@
+require 'serverspec'
+
+set :backend, :exec
+set :path, '/bin:/usr/bin:/sbin/usr/sbin'
+
+describe file('/etc/cumulus') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file('/etc/cumulus/ports.conf') do
+  it { should exist }
+  it { should be_file }
+  its(:content) { should match(/# Managed by Chef/) }
+  its(:content) { should match(/1=10G/) }
+  its(:content) { should match(/3=40G/) }
+  its(:content) { should match(/5=40G/) }
+  its(:content) { should match(/10=40G/) }
+  its(:content) { should match(/12=40G/) }
+  its(:content) { should match(/15=40G\/4/) }
+  its(:content) { should match(/16=40G\/4/) }
+  its(:content) { should match(/20=4x10G/) }
+  its(:content) { should match(/32=4x10G/) }
+end
