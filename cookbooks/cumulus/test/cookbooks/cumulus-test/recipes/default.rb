f027645cdecfb86b4e7b1335deb888ddfe3d0f53
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-02-11 13:24:51 +0000

    Set up a 'fake' Cumulus Linux environment
    Create some minimal skeleton configuration so the test-kitchen instance is at
    least similar enough to Cumulus Linux that the providers work and can be
    tested.

--- cookbooks/cumulus/test/cookbooks/cumulus-test/recipes/default.rb
+++ cookbooks/cumulus/test/cookbooks/cumulus-test/recipes/default.rb
@@ -7,6 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Setup a skeleton fake Cumulus Linux environment
+directory '/usr/cumulus/bin' do
+  recursive true
+end
+
+directory '/etc/cumulus' do
+end
+
+file '/usr/cumulus/bin/cl-license' do
+  content '#!/bin/sh
+    echo "Rocket Turtle!\n$0 $@" > /etc/cumulus/.license.txt'
+  mode '0755'
+end
+
+# Invoke the providers
 cumulus_ports 'speeds' do
   speed_10g ['swp1']
   speed_40g ['swp3','swp5-10', 'swp12']
