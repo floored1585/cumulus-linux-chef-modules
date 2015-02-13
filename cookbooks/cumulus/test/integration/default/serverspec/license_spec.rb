761965e9f3d9a757deaa48a47525424b66e91228
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-02-13 17:03:31 +0000

    Fix some cops

--- cookbooks/cumulus/test/integration/default/serverspec/license_spec.rb
+++ cookbooks/cumulus/test/integration/default/serverspec/license_spec.rb
@@ -6,5 +6,5 @@ set :path, '/bin:/usr/bin:/sbin/usr/sbin'
 describe file('/etc/cumulus/.license.txt') do
   it { should be_file }
   its(:content) { should match(/Rocket Turtle!/) }
-  its(:content) { should match(/\/usr\/cumulus\/bin\/cl-license -i http:\/\/localhost\/test.lic/) }
-end 
+  its(:content) { should match(%r{/usr/cumulus/bin/cl-license -i http://localhost/test.lic}) }
+end
