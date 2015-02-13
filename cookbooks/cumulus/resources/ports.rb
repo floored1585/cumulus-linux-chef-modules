46705eb331128d017686772cb82ee410d5857321
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-02-13 16:57:31 +0000

    Use new hash syntax

--- cookbooks/cumulus/resources/ports.rb
+++ cookbooks/cumulus/resources/ports.rb
@@ -1,8 +1,8 @@
 actions :create
 default_action :create
 
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :speed_10g, :kind_of => Array, :default => []
-attribute :speed_40g, :kind_of => Array, :default => []
-attribute :speed_40g_div_4, :kind_of => Array, :default => []
-attribute :speed_4_by_10g, :kind_of => Array, :default => []
+attribute :name, kind_of: String, name_attribute: true
+attribute :speed_10g, kind_of: Array, default: []
+attribute :speed_40g, kind_of: Array, default: []
+attribute :speed_40g_div_4, kind_of: Array, default: []
+attribute :speed_4_by_10g, kind_of: Array, default: []
