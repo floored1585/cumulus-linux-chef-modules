3ffa4e8fb87aa2532a38518301a9929174f29779
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-03-05 13:48:26 +0000

    Add a cumulus_bond test recipe

---
+++ cookbooks/cumulus/test/cookbooks/cumulus-test/recipes/bonds.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: cumulus-test
+# Recipe:: bonds
+#
+# Copyright 2015, Cumulus Networks
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cumulus_bond 'bond0' do
+  slaves ['swp1-2', 'swp4']
+  clag_id 42
+end
