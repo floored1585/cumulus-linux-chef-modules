2a7138c0a4a96f862ae0e1c030562c4b28110e12
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-03-06 14:59:01 +0000

    Add tests for bridges provider

---
+++ cookbooks/cumulus/test/cookbooks/cumulus-test/recipes/bridges.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: cumulus-test
+# Recipe:: bridges
+#
+# Copyright 2015, Cumulus Networks
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Classic bridge driver with defaults
+cumulus_bridge 'br0' do
+  ports ['swp10-11']
+end
+
+# Classic bridge driver over-ride all defaults
+cumulus_bridge 'br1' do
+  ports ['swp12-13']
+  ipv4 ['10.0.0.1/24', '192.168.1.0/16']
+  ipv6 ['2001:db8:abcd::/48']
+  # alias 'classic bridge number 1'
+  mtu 9000
+  stp false
+  mstpctl_treeprio 4096
+end
+
+# New bridge driver with defaults
+cumulus_bridge 'bridge2' do
+  ports ['swp15-16']
+  vlan_aware true 
+end
+
+# New bridge driver over-ride all defaults
+cumulus_bridge 'bridge3' do
+  ports ['swp17-18']
+  vlan_aware true
+  vids ['1-4094']
+  pvid 1
+  ipv4 ['10.0.100.1/24', '192.168.100.0/16']
+  ipv6 ['2001:db8:1234::/48']
+  # alias 'new bridge number 3'
+  mtu 9000
+  stp false
+  mstpctl_treeprio 4096
+end
