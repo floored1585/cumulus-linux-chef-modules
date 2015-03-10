7958c73681c8879c60490bf9cff7a6b19380fb85
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-03-10 16:19:41 +0000

    Test cumulus_interface
    Test the basic (accept all defaults) and complex (over-ride all defaults)
    invocations for the cumulus_interface provider.

--- cookbooks/cumulus/test/integration/default/serverspec/interfaces_spec.rb
+++ cookbooks/cumulus/test/integration/default/serverspec/interfaces_spec.rb
@@ -32,3 +32,23 @@ end
 describe file("#{intf_dir}/swp99") do
   it { should_not be_file }
 end
+
+# Should have been configured by interface
+describe file("#{intf_dir}/swp1") do
+  its(:content) { should match(/iface swp1/) }
+end
+
+describe file("#{intf_dir}/swp2") do
+  its(:content) { should match(/iface swp2 inet static/) }
+  its(:content) { should match(/address 192.168.200.1/) }
+  its(:content) { should match(/address 2001:db8:5678::/) }
+  its(:content) { should match(/mtu 9000/) }
+  its(:content) { should match(/bridge-vids 1-4094/) }
+  its(:content) { should match(/bridge-pvid 1/) }
+  its(:content) { should match(/link-speed 1000/) }
+  its(:content) { should match(/link-duplex full/) }
+  its(:content) { should match(/alias "interface swp2"/) }
+  its(:content) { should match(/mstpctl-portnetwork yes/) }
+  its(:content) { should match(/mstpctl-bpduguard yes/) }
+  its(:content) { should match(/address-virtual/) }
+end
