0b360b660672bfd8bd636ae3a53d7e43e395574b
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-03-30 13:46:41 +0100

    Update metadata to stage for release
    Update the license
    Fix maintainer_email
    Add a source_url

--- cookbooks/cumulus/metadata.rb
+++ cookbooks/cumulus/metadata.rb
@@ -1,7 +1,8 @@
 name             'cumulus'
 maintainer       'Cumulus Networks'
-maintainer_email 'ce-ceng@cumulusnetwosk.com'
-license          'All rights reserved'
+maintainer_email 'ce-ceng@cumulusnetworks.com'
+license          'Apache v2.0'
 description      'Manage Cumulus Networks specific configuration'
+source_url       'https://github.com/CumulusNetworks/cumulus-linux-chef-modules'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
