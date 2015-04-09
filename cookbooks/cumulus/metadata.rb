a02a0f55d7b070c9aabc9d27ed3b9f6fbadae150
James Findley <james.findley@digitalwindow.com>
2015-04-09 13:18:51 +0100

    Fix compatibility with chef < 12

--- cookbooks/cumulus/metadata.rb
+++ cookbooks/cumulus/metadata.rb
@@ -3,6 +3,6 @@ maintainer       'Cumulus Networks'
 maintainer_email 'ce-ceng@cumulusnetworks.com'
 license          'Apache v2.0'
 description      'Manage Cumulus Networks specific configuration'
-source_url       'https://github.com/CumulusNetworks/cumulus-linux-chef-modules'
+source_url       'https://github.com/CumulusNetworks/cumulus-linux-chef-modules' if respond_to?(:source_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
