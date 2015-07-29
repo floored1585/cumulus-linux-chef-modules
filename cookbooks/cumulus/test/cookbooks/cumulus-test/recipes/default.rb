98e851e3306c249918a6dea27090ef290edc57e1
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-07-29 12:19:49 +0100

    Use Cumulux VX & install Chef using APT
    Use the standard Cumulus VX box and Vagrant provisioning to install Chef with
    apt instead of the Omnibus installer.
    Fix a couple of the tests where the bridges used overlapping ports, which
    causes a warning from ifquery.

--- cookbooks/cumulus/test/cookbooks/cumulus-test/recipes/default.rb
+++ cookbooks/cumulus/test/cookbooks/cumulus-test/recipes/default.rb
@@ -8,33 +8,10 @@
 #
 
 # Setup a skeleton fake Cumulus Linux environment
-%w( /etc/cumulus /usr/cumulus/bin /etc/network/ifupdown2/templates ).each do |cldir|
-  directory cldir do
-    recursive true
-  end
-end
-
 file '/usr/cumulus/bin/cl-license' do
   content '#!/bin/sh
     echo "Rocket Turtle!\nexpires=$(date +%s)\n$0 $@" > /etc/cumulus/.license.txt'
   mode '0755'
-end
-
-# Setup the Cumulus repository and install ifupdown2
-execute 'apt-update' do
-  command 'apt-get update'
-  action :nothing
-end
-
-file '/etc/apt/sources.list.d/cumulus.list' do
-  content 'deb [ arch=amd64 ] http://repo.cumulusnetworks.com CumulusLinux-2.5 main'
-  notifies :run, 'execute[apt-update]', :immediately
-end
-
-%w( python-ifupdown2 python-argcomplete python-ipaddr ).each do |pkg|
-  apt_package pkg do
-    options '--force-yes'
-  end
 end
 
 include_recipe "cumulus-test::ports"
