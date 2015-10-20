360ee707f3fc50b679aeeb853f17c344003f81dd
Ian Clark <ian@f85.net>
2015-10-20 00:16:36 -0700

    Fixing ifquery -o json comparison in interface and bridge (for proper notify behavior)

--- cookbooks/cumulus/metadata.rb
+++ cookbooks/cumulus/metadata.rb
@@ -5,4 +5,4 @@ license          'Apache v2.0'
 description      'Manage Cumulus Networks specific configuration'
 source_url       'https://github.com/CumulusNetworks/cumulus-linux-chef-modules' if respond_to?(:source_url)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.2.2'
